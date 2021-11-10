7586ec2457de19b8aa51678255f0586c43752bf1
Guigxs <guigxs19@gmail.com>
2021-11-10 15:57:40 +0100

    Feat: mandelbrot computation

--- server/index.py
+++ server/index.py
@@ -1,10 +1,17 @@
-from bottle import run, get, post, request
+from bottle import run, get, post, request, response
+import json
+from compute import compute
 
 @get('/')
 def index():
-    complex_r = request.params.get("complex_r")
-    complex_i = request.params.get("complex_i")
-    iter = request.params.get("iter")
-    return f'<b>Hello Guillaume, your complex is : {complex_r} + {complex_i}j with {iter} iterations </b>!'
+    complex_r = float(request.params.get("complex_r"))
+    complex_i = float(request.params.get("complex_i"))
+    iter = int(request.params.get("iter"))
+
+    result = compute(complex_r, complex_i, iter)
+    response.content_type = 'application/json'
+    
+    return json.dumps({"response":result})
+    # f'<b>Hello Guillaume, your complex is : {complex_r} + {complex_i}j with {iter} iterations </b>!<p> Result : {result} </p>'
 
 run(host='0.0.0.0', port=8181)
